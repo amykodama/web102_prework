337fdebd188666a042ff815680f336191c7cb036
Amy Kodama <amy.kodamas2@gmail.com>
2025-04-23 23:28:57 -0300

    Updated style.css and index.js

--- index.js
+++ index.js
@@ -25,31 +25,38 @@ function deleteChildElements(parent) {
 // grab the element with the id games-container
 const gamesContainer = document.getElementById("games-container");
 
-// create a function that adds all data from the games array to the page
+// create a function that adds all dat   from the games array to the page
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < num_games; i++) {
 
         // create a new div element, which will become the game card
+        const gameCard = document.createElement("div");
 
 
         // add the class game-card to the list
+        gameCard.classList.add("game-card");
 
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
+        gameCard.innerHTML = `
+        <img class="game-img" src="${game.img}" alt="${game.name}" />
+        <h3>${game.name}</h3>
+        <p>${game.description}</p>
+    `;
         // append the game to the games-container
+        gamesContainer.appendChild(gameCard);
+    }
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
