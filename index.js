af3f34bcea5fa5d96faaa7bb8e9e2e796c2e82d1
Amy Kodama <amy.kodamas2@gmail.com>
2025-04-26 14:13:40 -0300

    challenge 3
    changed style.css and index.js

--- index.js
+++ index.js
@@ -34,10 +34,8 @@ function addGamesToPage(games) {
         // create a new div element, which will become the game card
         const gameCard = document.createElement("div");
 
-
         // add the class game-card to the list
         gameCard.classList.add("game-card");
-
 
         // set the inner HTML using a template literal to display some info 
         // about each game
@@ -57,7 +55,7 @@ function addGamesToPage(games) {
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
 addGamesToPage(GAMES_JSON);
-
+ 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
  * total number of contributions, amount donated, and number of games on the site.
